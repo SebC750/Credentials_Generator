e8bfea542a082a3a53e6c2ed781b8fa41e461aa9
SebC750 <SebC89999@gmail.com>
2023-10-04 17:17:59 -0400

    git commit

--- CredentialsMenu.java
+++ CredentialsMenu.java
@@ -1,22 +1,30 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 public class CredentialsMenu {
-	JFrame mainMenu = new JFrame("Credentials Generator");
+	JFrame mainMenu = new JFrame("Credentials Generator and Manager");
 
     JPanel title = new JPanel();
     JPanel inputTextPanel = new JPanel();
     JPanel exitPanel = new JPanel();
+    JPanel usernamePanel = new JPanel();
+    JPanel passwordPanel = new JPanel();
 
     JLabel welcomeText = new JLabel("Welcome to the credentials randomizer program");
     JLabel inputPromptText = new JLabel("Click on the randomizer buttons to get a new username and password");
-
     JLabel userNameText = new JLabel("Username: ");
+    JLabel passwordText = new JLabel("Password: ");
+
+    JButton userNameRandomButton = new JButton("Randomize");
+    JButton passwordRandomButton = new JButton("Randomize");
     JButton exitButton = new JButton("Exit Program");
     
+    Border inputBorder = BorderFactory.createLineBorder(new Color(20,190,255),5);
     
     public CredentialsMenu() {
     	mainMenu.setSize(600, 600);
@@ -26,33 +34,52 @@ public class CredentialsMenu {
         mainMenu.setLayout(null);
         mainMenu.getContentPane().setBackground(new Color(100, 55, 90));
         
+
         title.setBounds(0, 0, 600, 50);
         exitPanel.setBounds(420,500,150,50);
         inputTextPanel.setBounds(0, 50, 600, 150);
-
+        inputTextPanel.setBorder(inputBorder);
+        usernamePanel.setBounds(-50, 100, 400, 20);
+        passwordPanel.setBounds(-50, 150, 400, 20);
+ 
         welcomeText.setFont(new Font("Monospaced", Font.BOLD, 16));
         welcomeText.setForeground(Color.white);
         inputPromptText.setFont(new Font("Monospaced", Font.BOLD, 10));
         inputPromptText.setForeground(Color.white);
-
+        userNameText.setFont(new Font("Monospaced", Font.BOLD, 10));
+        userNameText.setForeground(Color.white);
+        passwordText.setFont(new Font("Monospaced", Font.BOLD, 10));
+        passwordText.setForeground(Color.white);
 
         title.setBackground(new Color(150, 55, 120));
         exitPanel.setBackground(new Color(150, 55, 120));
         inputTextPanel.setBackground(new Color(100, 55, 90));
-                 
+        usernamePanel.setBackground(new Color(100, 55, 90));
+        passwordPanel.setBackground(new Color(100, 55, 90));
+
+
         exitButton.setOpaque(false);
         exitButton.setContentAreaFilled(false);
         exitButton.setBorderPainted(false);
         
+
         ActionListener exitProgram = new ClickListener();
         exitButton.addActionListener(exitProgram);
         exitButton.addActionListener(e -> {
             mainMenu.dispose();
         });
+
+
         exitPanel.add(exitButton);
         title.add(welcomeText, BorderLayout.CENTER);
         inputTextPanel.add(inputPromptText);
-        
+        usernamePanel.add(userNameText);
+        passwordPanel.add(passwordText);
+        inputTextPanel.add(usernamePanel);
+        inputTextPanel.add(passwordPanel);
+
+        mainMenu.add(usernamePanel);
+        mainMenu.add(passwordPanel);
         mainMenu.add(inputTextPanel);
         mainMenu.add(title);
         mainMenu.add(exitPanel);
